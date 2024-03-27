da6c8a2eaf29b0fabdbd52066a560aaaa9cebedc
Aliu Gloria <glorysamuel75@gmail.com>
2024-03-27 19:59:29 +0100

    update service pages

--- src/components/reuseable/serviceSolution/index.tsx
+++ src/components/reuseable/serviceSolution/index.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const ServiceSolution = () => {
   return (
-    <div className='bg-[#2A317F]'>
-        
-        ServiceSolution
-        
-        </div>
-  )
-}
+    <div className="font-serif w-full h-full root_parent_wrapper bg-[#2A317F]">
+      <div className="root_child_wrapper">ServiceSolution</div>
+    </div>
+  );
+};
 
 export default ServiceSolution;
