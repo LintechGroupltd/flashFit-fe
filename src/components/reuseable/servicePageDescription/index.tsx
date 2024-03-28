c0b7fb2df9004d63f7db71b5664a8a3756b57509
Josiah Arkson <158191169+josiah-lintechgroup@users.noreply.github.com>
2024-03-28 10:50:49 +0100

    fix resposiveness

--- src/components/reuseable/servicePageDescription/index.tsx
+++ src/components/reuseable/servicePageDescription/index.tsx
@@ -12,7 +12,7 @@ const ServicePageDesc = ({ title, desc1, desc2 }: props) => {
       className="font-serif root_parent_wrapper"
       style={{ color: "#0F0F0F" }}>
       <div className="root_child_wrapper">
-        <div className="w-full flex flex-col lg:flex-row justify-between gap-10 py-[50px] sm:py-24">
+        <div className="w-full flex flex-col lg:flex-row justify-between gap-10 pt-[30px] pb-[40px] sm:pt-24 sm:pb-24">
           <h1 className=" text-3xl sm:text-5xl w-full lg:max-w-sm font-semibold lg:text-left text-center">
             {title}
           </h1>
