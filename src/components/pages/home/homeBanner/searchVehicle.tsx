594d13b54c856db6da319349797d3f1e3acf5a5d
Josiah Arkson <158191169+josiah-lintechgroup@users.noreply.github.com>
2024-03-28 16:09:03 +0100

    update hoempage

--- src/components/pages/home/homeBanner/searchVehicle.tsx
+++ src/components/pages/home/homeBanner/searchVehicle.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import searchVehicleDownArrow from '../../../../../public/assets/home/serachVehicle/searchVehicledownArrow.svg'
+import searchVehicleDownArrow from "../../../../../public/assets/home/serachVehicle/searchVehicledownArrow.svg";
 import Image from "next/image";
 
 const data = [
@@ -7,11 +7,9 @@ const data = [
   { id: "2", title: "Search by Tyre Size" },
   // { id: "3", mobileTitle: "Vehicle" },
   // { id: "4", mobileTitle: "Tyre Size" },
- 
 ];
 
-
-const options = ['Tyres', 'Others'];
+const options = ["Tyres", "Others"];
 
 function SearchVehicle() {
   const [searchType, setSearchType] = useState(data[0]?.id);
@@ -28,8 +26,7 @@ function SearchVehicle() {
       style={{
         boxShadow: "0px 4px 40px 0px rgba(0, 0, 0, 0.12)",
       }}
-      className="bg-[#fff] z-0 shadow-lg  w-full overflow-hidden rounded-[24px] min-h-[180px] md:translate-y-[-50%] translate-y-[-30%] mx-auto " // Add relative positioning here
-       
+      className="bg-[#fff] z-0 shadow-lg  w-full overflow-hidden rounded-[24px] min-h-[180px]  translate-y-[-50%] mx-auto " // Add relative positioning here
     >
       <div className="flex bg-[#a4a7c8] h-[56px]">
         {data.map((item) => {
@@ -42,10 +39,9 @@ function SearchVehicle() {
                 isActive
                   ? "w-auto h-full px-[24px] bg-white text-[#2A317F] font-serif font-semibold flex gap-[12px] items-center justify-center"
                   : "w-auto h-full px-[24px] bg-inherit text-[#ffffff] font-serif font-semibold flex gap-[12px] items-center justify-center"
-              }
-            >
+              }>
               <span className="font-serif text-[14px]">{item?.title}</span>
-          
+
               <img
                 className="md:w-[20px] md:h-[20px] w-[15px] h-[15px] object-cover"
                 src={
@@ -65,7 +61,7 @@ function SearchVehicle() {
             What are you looking for?
           </label>
           <input
-            className="h-[48px] border border-grey-500 rounded-[8px] pl-4 pr-12 focus:outline-none font-sans" 
+            className="h-[48px] border border-grey-500 rounded-[8px] pl-4 pr-12 focus:outline-none font-sans"
             value={selectedOption}
             readOnly
             onClick={() => setIsDropdownOpen(!isDropdownOpen)}
@@ -79,45 +75,46 @@ function SearchVehicle() {
               <div
                 key={option}
                 className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                onClick={() => handleOptionSelect(option)}
-              >
+                onClick={() => handleOptionSelect(option)}>
                 {option}
               </div>
             ))}
           </div>
           <div className="absolute top-[52px] right-2 flex items-center pr-3 pointer-events-none">
-            <Image src={searchVehicleDownArrow} alt=""/>
+            <Image src={searchVehicleDownArrow} alt="" />
           </div>
         </div>
 
         <div className="relative flex flex-col flex-1 gap-[5px] md:mt-0 mt-5">
-           <label className="text-[#000000]  font-semibold font-sans mb-[5px] text-[14px]">
-             Vehicle Registration
-           </label>
-           <input className="h-[48px] font-sans text-[16px] border border-grey-500 rounded-[8px] pl-14 focus:outline-none"
-              placeholder="Enter Number"
-           />
-           <div className="bg-[#2A317F] w-[40px] absolute top-[32px] py-[11px] rounded-tl-[8px] rounded-bl-[8px]">
-             <h2 className="text-[#fff] text-[16px] font-[500]  text-center font-sans">UK</h2>
-           </div>
-         </div>
+          <label className="text-[#000000]  font-semibold font-sans mb-[5px] text-[14px]">
+            Vehicle Registration
+          </label>
+          <input
+            className="h-[48px] font-sans text-[16px] border border-grey-500 rounded-[8px] pl-14 focus:outline-none"
+            placeholder="Enter Number"
+          />
+          <div className="bg-[#2A317F] w-[40px] absolute top-[32px] py-[11px] rounded-tl-[8px] rounded-bl-[8px]">
+            <h2 className="text-[#fff] text-[16px] font-[500]  text-center font-sans">
+              UK
+            </h2>
+          </div>
+        </div>
 
-         <div className="flex flex-col flex-1 gap-[5px] md:mt-0 mt-5">
-           <label className="text-[#000000] font-semibold font-sans mb-[5px] text-[14px]">
-             Postcode
-           </label>
-           <input className="h-[48px] border border-grey-500 rounded-[8px] font-sans pl-3 focus:outline-none" 
-             placeholder="Enter Code"
-           />
-          
-         </div>
+        <div className="flex flex-col flex-1 gap-[5px] md:mt-0 mt-5">
+          <label className="text-[#000000] font-semibold font-sans mb-[5px] text-[14px]">
+            Postcode
+          </label>
+          <input
+            className="h-[48px] border border-grey-500 rounded-[8px] font-sans pl-3 focus:outline-none"
+            placeholder="Enter Code"
+          />
+        </div>
 
-         <div className="flex flex-col flex-1 gap-[5px] mt-[31px]">
-           <button className="bg-[#F8AD39] h-[48px] text-[#fff] rounded-[8px] font-[600] text-[16px] font-sans">
-              Search
-           </button>
-         </div>
-       
+        <div className="flex flex-col flex-1 gap-[5px] mt-[31px]">
+          <button className="bg-[#F8AD39] h-[48px] text-[#fff] rounded-[8px] font-[600] text-[16px] font-sans">
+            Search
+          </button>
+        </div>
       </div>
     </div>
   );
