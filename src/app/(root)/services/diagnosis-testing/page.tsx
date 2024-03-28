2a3d50b24e5f2c57538c6b0283ba20ab0290963e
Josiah Arkson <158191169+josiah-lintechgroup@users.noreply.github.com>
2024-03-28 10:21:41 +0100

    updates

---
+++ src/app/(root)/services/diagnosis-testing/page.tsx
@@ -0,0 +1,12 @@
+import DiagnosisTesting from "@/components/pages/diagnosis-testing";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <DiagnosisTesting />
+    </div>
+  );
+};
+
+export default page;
