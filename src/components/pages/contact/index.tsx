e2aaabce9c0ecef38153b4cb7a43d4a421ada97d
Aliu Gloria <glorysamuel75@gmail.com>
2024-03-29 18:10:07 +0100

    contact close btn updated

--- src/components/pages/contact/index.tsx
+++ src/components/pages/contact/index.tsx
@@ -7,13 +7,13 @@ import ContactServiceType from "./contactServiceType";
 import ContactForm from "./contactForm";
 import ContactSuccessMessage from "./contactSuccessMessage";
 import Link from "next/link";
- import styles from "./styles.module.css";
+import styles from "./styles.module.css";
 
 const ContactPage = () => {
   const [activeStep, setActiveStep] = useState(0);
   const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
 
-  const handleStepChange = (step:number) => {
+  const handleStepChange = (step: number) => {
     setActiveStep(step);
   };
 
@@ -23,23 +23,17 @@ const ContactPage = () => {
 
   const totalSteps = 2;
   // const progressWidth = (activeStep / (totalSteps - 1)) * 100;
-  const progressWidth = (activeStep === 0 ? 50 : 100);
-
-
-
-
-
- 
+  const progressWidth = activeStep === 0 ? 50 : 100;
 
   const handleBack = () => {
     if (activeStep <= 0) {
-      return;}
+      return;
+    }
     setActiveStep(activeStep - 1);
     if (activeStep === 1) {
       setSelectedOptions([]);
     }
   };
-  
 
   return (
     <div
@@ -48,86 +42,119 @@ const ContactPage = () => {
     >
       <div className="contactNav w-full flex items-center justify-between">
         <div>
-          {(activeStep <= 0 || activeStep === 2) &&  (<div className=" " data-aos='fade-right'><Link href={'/'}> <FlashLogo /> </Link></div>)}
-     
- 
-             {(activeStep !== 0 && activeStep !== 2 )  && ( 
-             <div className={`${styles.arrowMain} border px-2 py-5 rounded-full cursor-pointer`} onClick={handleBack}> 
-            <div className={`${styles.arrowWrapper}`}>
-            <span className={`${styles.arrowSpan}`}>
-               <div  className={`${styles.arr1} flex items-center justify-center`}>
-                <RoundedBackBtn /> 
-                </div>
-               <div className={`${styles.arr2} flex items-center justify-center `}>
-                <RoundedBackBtn /> 
-                </div>
-            </span>
-          </div>
-          </div> 
+          {(activeStep <= 0 || activeStep === 2) && (
+            <div className=" " data-aos="fade-right">
+              <Link href={"/"}>
+                {" "}
+                <FlashLogo />{" "}
+              </Link>
+            </div>
+          )}
+
+          {activeStep !== 0 && activeStep !== 2 && (
+            <div
+              className={`${styles.arrowMain} border px-2 py-5 rounded-full cursor-pointer`}
+              onClick={handleBack}
+            >
+              <div className={`${styles.arrowWrapper}`}>
+                <span className={`${styles.arrowSpan}`}>
+                  <div
+                    className={`${styles.arr1} flex items-center justify-center`}
+                  >
+                    <RoundedBackBtn />
+                  </div>
+                  <div
+                    className={`${styles.arr2} flex items-center justify-center `}
+                  >
+                    <RoundedBackBtn />
+                  </div>
+                </span>
+              </div>
+            </div>
           )}
         </div>
 
+        
 
-         
-       
-       
-
-
-        <div className={`${styles.arrowMain} border px-3 py-5 rounded-full cursor-pointer`}> 
-        <Link href={'/'}>
+        <Link href={"/"}>
+          <div
+            className={`${styles.arrowMain} border px-3 py-5 rounded-full cursor-pointer`}
+          >
             <div className={`${styles.arrowWrapper}`}>
-            <span className={`${styles.arrowSpan}`}>
-               <div  className={`${styles.arr1} flex items-center justify-center`}>
-                <CloseNavIcon /> 
+              <span className={`${styles.arrowSpan}`}>
+                <div
+                  className={`${styles.arr1} flex items-center justify-center`}
+                >
+                  <CloseNavIcon />
                 </div>
 
-               <div className={`${styles.arr2} flex items-center justify-center `}>
-                <CloseNavIcon /> 
+                <div
+                  className={`${styles.arr2} flex items-center justify-center `}
+                >
+                  <CloseNavIcon />
                 </div>
-            </span>
+              </span>
+            </div>
           </div>
-          </Link>
-          </div> 
+        </Link>
       </div>
 
 
-     
-      {activeStep !== 2 && <div className="absolute top-28 lg:top-10 left-1/2 -translate-x-1/2 w-auto flex items-center justify-center">
+
+
+      {activeStep !== 2 && (
+        <div className="absolute top-28 lg:top-10 left-1/2 -translate-x-1/2 w-auto flex items-center justify-center">
           {selectedOptions.map((option, index) => (
             <button
               type="button"
               key={index}
               className="  thin flex items-center justify-center gap-2  text-xs sm:text-base cursor-pointer px-4 py-4 rounded-lg bg-white bg-opacity-10 border border-white"
-              data-aos="fade-up-right" >
-              {option} 
+              data-aos="fade-up-right"
+            >
+              {option}
             </button>
           ))}
-        </div>}
+        </div>
+      )}
 
-
-
-
-      {(activeStep > 0 && activeStep !== totalSteps - 1) && ( <div className="absolute bottom-4" data-aos='fade-right'> <FlashLogo /> </div> )}
-
-
+      {activeStep > 0 && activeStep !== totalSteps - 1 && (
+        <div className="absolute bottom-4" data-aos="fade-right">
+          {" "}
+          <FlashLogo />{" "}
+        </div>
+      )}
 
       <div className="w-full h-full flex items-center justify-center">
         <>
-          {activeStep === 0 && ( <ContactServiceType handleStepChange={handleStepChange} handleOptionSelect={handleOptionSelect} /> )}
-          {activeStep === 1 && ( <ContactForm handleStepChange={handleStepChange} /> )}
+          {activeStep === 0 && (
+            <ContactServiceType
+              handleStepChange={handleStepChange}
+              handleOptionSelect={handleOptionSelect}
+            />
+          )}
+          {activeStep === 1 && (
+            <ContactForm handleStepChange={handleStepChange} />
+          )}
           {activeStep === 2 && <ContactSuccessMessage />}
         </>
       </div>
 
- 
       {/*________________________progress bar ____________________*/}
-    { activeStep !== 2 &&  <div className="w-full flex items-center">
-        <div className="progress-bar w-full flex items-center  justify-center">
-          <div className="progress-step w-2/5  h-2 bg-gray-300 sm:w-[100%]] rounded-md">
-            <div className="active h-full rounded-md" style={{ width: `${progressWidth}%`, backgroundColor: "#F8AD39" }}></div>
+      {activeStep !== 2 && (
+        <div className="w-full flex items-center">
+          <div className="progress-bar w-full flex items-center  justify-center">
+            <div className="progress-step w-2/5  h-2 bg-gray-300 sm:w-[100%]] rounded-md">
+              <div
+                className="active h-full rounded-md"
+                style={{
+                  width: `${progressWidth}%`,
+                  backgroundColor: "#F8AD39",
+                }}
+              ></div>
+            </div>
           </div>
         </div>
-      </div>}
+      )}
     </div>
   );
 };
