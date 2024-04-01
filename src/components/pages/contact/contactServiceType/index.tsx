66575e626c5c3c5a9a315830f5063cd466745685
Aliu Gloria <glorysamuel75@gmail.com>
2024-04-01 14:56:05 +0100

    text changed on contact form

--- src/components/pages/contact/contactServiceType/index.tsx
+++ src/components/pages/contact/contactServiceType/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export const contactServiceOptions = [
   { id: 1, tag: "MOT Testing" },
-  { id: 2, tag: "Servicing" },
+  { id: 2, tag: "Services" },
   { id: 3, tag: "Brakes" },
   { id: 4, tag: "Exhaust" },
-  { id: 5, tag: "Tyres & Wheels" },
+  { id: 5, tag: "Tyres" },
   {id:6, tag: "wheels Alignments"},
   { id: 7, tag: "Diagnosis Testing" },
 ];
@@ -25,7 +25,7 @@ const ContactServiceType = ({handleStepChange,handleOptionSelect}:StepProps) => 
       <div>
         <ul className="flex items-center justify-center flex-wrap gap-4 sm:gap-10">
           {contactServiceOptions.map((item, idx) => (
-            <li key={idx} className="text-xs sm:text-base cursor-pointer px-4 py-4 md:px-10 md:py-6 rounded-lg bg-white bg-opacity-10 border border-white hover:bg-opacity-5"
+            <li key={idx} className="text-xs sm:text-base cursor-pointer px-4 py-4 md:px-10 rounded-lg bg-white bg-opacity-10 border border-white hover:bg-opacity-5"
              onClick={() =>{
               handleStepChange(1);
               handleOptionSelect(item.tag);
